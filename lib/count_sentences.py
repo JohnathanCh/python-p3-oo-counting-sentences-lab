833a01affd0720a3641b590c51b0f5108223ff5a
JohnathanCh <johnchriest@gmail.com>
2023-01-24 14:10:12 -0800

    done

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,40 @@
 #!/usr/bin/env python3
+import pdb
+import re
 
 class MyString:
-  pass
+  def __init__(self, value = None):
+    if value:
+      self._value = value
+  
+  def get_value(self):
+    return self._value if hasattr(self, '_value') else None
+  
+  def set_value(self, value):
+    if type(value) == str:
+      self._value = value
+    else:
+      self._value = None
+      print("The value must be a string.")
+    
+  value = property(get_value, set_value)
+    
+  def is_sentence(self):
+    return self._value.endswith(".")
+  
+  def is_question(self):
+    return self._value.endswith("?")
+  
+  def is_exclamation(self):
+    return self._value.endswith("!")
+  
+  def count_sentences(self):
+    if hasattr(self, "_value"):
+      sentences = re.split(r"\.+|\?+|\!+", getattr(self, "_value"))
+      results = list(filter(None, sentences))
+      # pdb.set_trace()
+      return len(results)
+    else:
+      return 0
+  
+  
